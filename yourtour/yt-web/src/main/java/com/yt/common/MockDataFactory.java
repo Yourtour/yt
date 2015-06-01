4664bfe2e5fc40932f03c5250f8083c04b7be630
unknown <zlpxjz>
2015-06-01 17:53:52 +0800

    在目录下添加package.java文件来描述每个package下代码用途

--- yourtour/yt-web/src/main/java/com/yt/common/MockDataFactory.java
+++ yourtour/yt-web/src/main/java/com/yt/common/MockDataFactory.java
@@ -6,6 +6,12 @@ import java.util.List;
 import com.yt.dal.hbase.BaseBean;
 
 public class MockDataFactory {
+	/**
+	 * 根据URL 中文件从读取模拟数据，并将模拟数据转换成对象
+	 * @param clazz
+	 * @param url
+	 * @return
+	 */
 	public static  BaseBean getMockData(Class<?> clazz, String url){
 		InputStream is = MockDataFactory.class.getClassLoader().getResourceAsStream(url + ".json");
 		
@@ -13,6 +19,12 @@ public class MockDataFactory {
 		return null;
 	}
 	
+	/**
+	 * 根据URL 中文件从读取模拟数据，并将模拟数据转换成集合对象
+	 * @param clazz
+	 * @param url
+	 * @return
+	 */
 	public static  List<? extends BaseBean> getMockListData(Class<?> clazz, String url){
 		return null;
 	}
---
+++ yourtour/yt-web/src/main/java/com/yt/service/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Tony.Zhang
+ *	该Package包含了平台的业务逻辑代码实现，分为Facade层和Service单元层
+ * Facade层主要负责将不同的Service实现按照业务逻辑进行编排实现。
+ * Service层主要负责关于具体某个功能点或者具体某个实体的业务实现。比如每个业务实体的CRUD 可以使用一个SERVICE实现。
+ */
+package com.yt.service;
