a2c49ebce4b8ce56523cd4d28157cee9b119d56e
John Peng <josh_73_9@hotmail.com>
2015-06-01 13:17:17 +0800

    修改了在CRUD单元测试中对表的存在性没有的判断的缺陷，完善了单元测试。

--- yourtour/yt-dal/src/test/java/com/yt/test/hbase/TestCrudGeneralOperate.java
+++ yourtour/yt-dal/src/test/java/com/yt/test/hbase/TestCrudGeneralOperate.java
@@ -16,8 +16,10 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.yt.dal.hbase.DdlGeneralOperate;
 import com.yt.dal.hbase.HbaseManager;
 import com.yt.dal.hbase.ICrudOperate;
+import com.yt.dal.hbase.IDdlOperate;
 import com.yt.test.hbase.bean.BaseServiceInfo;
 import com.yt.test.hbase.bean.TestBean;
 
@@ -25,6 +27,7 @@ public class TestCrudGeneralOperate {
 	private ApplicationContext context;
 	private HbaseManager manager;
 	private ICrudOperate crudOperate;
+	private IDdlOperate ddlOperate;
 
 	@Before
 	public void setUp() throws Exception {
@@ -32,6 +35,7 @@ public class TestCrudGeneralOperate {
 				new String[] { "hbaseApplicationContext.xml" });
 		manager = context.getBean(HbaseManager.class);
 		crudOperate = context.getBean(ICrudOperate.class);
+		ddlOperate = context.getBean(DdlGeneralOperate.class);
 	}
 
 	@After
@@ -52,12 +56,15 @@ public class TestCrudGeneralOperate {
 
 	@Test
 	public void testCrudTestBean() {
-		TestBean bean = new TestBean();
-		bean.setRowKey("row-key-001");
-		bean.setTableName("table name 03");
-		bean.setFamily("family 03");
-		bean.setQualifier("qualifier 03");
 		try {
+			if (!ddlOperate.tableExist(TestBean.class)) {
+				ddlOperate.createTable(TestBean.class);
+			}
+			TestBean bean = new TestBean();
+			bean.setRowKey("row-key-001");
+			bean.setTableName("table name 03");
+			bean.setFamily("family 03");
+			bean.setQualifier("qualifier 03");
 			crudOperate.save(bean);
 			TestBean bean1 = (TestBean) crudOperate.get(TestBean.class,
 					bean.getRowKey());
@@ -70,6 +77,10 @@ public class TestCrudGeneralOperate {
 			TestBean bean2 = (TestBean) crudOperate.get(TestBean.class,
 					bean.getRowKey());
 			assertNull(bean2);
+
+			if (ddlOperate.tableExist(TestBean.class)) {
+				ddlOperate.dropTable(TestBean.class);
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			fail(ex.getMessage());
@@ -78,31 +89,40 @@ public class TestCrudGeneralOperate {
 
 	@Test
 	public void testBaseServiceInfo() {
-		BaseServiceInfo info = new BaseServiceInfo();
-		info.setCode("code");
-		info.setCreatedUserId("john");
-		info.setMemo("memo");
-		info.setMode("mode");
-		info.setName("name");
-		info.setPrepayment(true);
-		info.setRowKey("rowkey");
-		info.setStatus(BaseServiceInfo.BaseServiceInfoEnum.CLOSED);
-		info.setType("type");
-		info.setUpdatedTime(System.currentTimeMillis());
-		info.setUpdatedUserId("john");
 		try {
+			if (!ddlOperate.tableExist(BaseServiceInfo.class)) {
+				ddlOperate.createTable(BaseServiceInfo.class);
+			}
+			BaseServiceInfo info = new BaseServiceInfo();
+			info.setCode("code");
+			info.setCreatedUserId("john");
+			info.setMemo("memo");
+			info.setMode("mode");
+			info.setName("name");
+			info.setPrepayment(true);
+			info.setRowKey("rowkey");
+			info.setStatus(BaseServiceInfo.BaseServiceInfoEnum.CLOSED);
+			info.setType("type");
+			info.setUpdatedTime(System.currentTimeMillis());
+			info.setUpdatedUserId("john");
 			crudOperate.save(info);
-			BaseServiceInfo info1 = (BaseServiceInfo)crudOperate.get(BaseServiceInfo.class, info.getRowKey());
+			BaseServiceInfo info1 = (BaseServiceInfo) crudOperate.get(
+					BaseServiceInfo.class, info.getRowKey());
 			assertNotNull(info1);
 			assertTrue(info.getRowKey().equals(info1.getRowKey()));
 			assertTrue(info.getName().equals(info1.getName()));
 			assertTrue(info.isPrepayment() == info1.isPrepayment());
 			assertNotNull(info1.getStatus());
 			assertTrue(info.getStatus().name().equals(info1.getStatus().name()));
-			
+
 			crudOperate.deleteRow(info);
-			BaseServiceInfo info2 = (BaseServiceInfo)crudOperate.get(BaseServiceInfo.class, info.getRowKey());
+			BaseServiceInfo info2 = (BaseServiceInfo) crudOperate.get(
+					BaseServiceInfo.class, info.getRowKey());
 			assertNull(info2);
+
+			if (ddlOperate.tableExist(BaseServiceInfo.class)) {
+				ddlOperate.dropTable(BaseServiceInfo.class);
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			fail(ex.getMessage());
