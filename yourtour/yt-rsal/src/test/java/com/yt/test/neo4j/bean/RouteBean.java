6ab26b8eb66a6ba75f76c66cd8df5dcd26be0ec9
John Peng <josh_73_9@hotmail.com>
2015-06-22 11:34:59 +0800

    对Neo4j的CRUD进行了单元测试，并对基本关系进行了单元测试。尚未完成对关系查询方法的支持。

---
+++ yourtour/yt-rsal/src/test/java/com/yt/test/neo4j/bean/RouteBean.java
@@ -0,0 +1,167 @@
+package com.yt.test.neo4j.bean;
+
+import org.springframework.data.neo4j.annotation.GraphProperty;
+
+import com.yt.dal.hbase.annotation.HbaseColumn;
+import com.yt.dal.hbase.annotation.HbaseTable;
+import com.yt.rsal.neo4j.bean.Neo4JBaseBean;
+import com.yt.test.neo4j.bean.Constants.Status;
+
+/**
+ * @author Tony.Zhang
+ * 
+ *         行程bean，定义了行程基本信息
+ * 
+ */
+@HbaseTable(name = "T_ROUTE_INFO")
+public class RouteBean extends Neo4JBaseBean {
+	private static final long serialVersionUID = -8980153602025087935L;
+
+	private @HbaseColumn(name = "name")
+	@GraphProperty
+	String name; // 安排名称
+	private @HbaseColumn(name = "iu")
+	transient String imageUrl; // 行程图片
+	private @HbaseColumn(name = "intr")
+	transient String intro; // 概述， 可以针对行程安排中具体某天或者某个景点进行描述
+	private @HbaseColumn(name = "feat")
+	transient String feature; // 特点， 可以针对行程安排中具体某天或者某个景点进行特点描述
+	private @HbaseColumn(name = "reas")
+	transient String reason; // 推荐理由，， 可以针对行程安排中具体某天或者某个景点进行推荐描述
+	private @HbaseColumn(name = "plac")
+	@GraphProperty
+	String place; // 目的地
+	private @HbaseColumn(name = "st")
+	transient long startTime; // 安排开始时间，以秒为单位
+	private @HbaseColumn(name = "et")
+	transient long endTime; // 安排结束时间，以秒为单位
+	private @HbaseColumn(name = "peri")
+	transient int period; // 安排持续时间， 以秒为单位
+	private @HbaseColumn(name = "cuid")
+	transient String createdUserId = "";
+	private @HbaseColumn(name = "ct")
+	transient long createdTime;
+	private @HbaseColumn(name = "uuid")
+	transient String updatedUserId = "";
+	private @HbaseColumn(name = "ut")
+	transient long updatedTime;
+	private @HbaseColumn(name = "stat")
+	@GraphProperty
+	Status status;
+
+	public RouteBean() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getPlace() {
+		return this.place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	public String getFeature() {
+		return feature;
+	}
+
+	public void setFeature(String feature) {
+		this.feature = feature;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public String getCreatedUserId() {
+		return createdUserId;
+	}
+
+	public void setCreatedUserId(String createdUserId) {
+		this.createdUserId = createdUserId;
+	}
+
+	public long getCreatedTime() {
+		return createdTime;
+	}
+
+	public void setCreatedTime(long createdTime) {
+		this.createdTime = createdTime;
+	}
+
+	public String getUpdatedUserId() {
+		return updatedUserId;
+	}
+
+	public void setUpdatedUserId(String updatedUserId) {
+		this.updatedUserId = updatedUserId;
+	}
+
+	public long getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(long updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+}
