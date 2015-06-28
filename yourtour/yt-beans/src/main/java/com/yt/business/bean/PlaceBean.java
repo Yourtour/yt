95a868ed057dbbc9dd2e2ec093df4b5bacc4b79c
John Peng <josh_73_9@hotmail.com>
2015-06-28 14:59:13 +0800

    修改了两个实体对象。

--- yourtour/yt-beans/src/main/java/com/yt/business/bean/PlaceBean.java
+++ yourtour/yt-beans/src/main/java/com/yt/business/bean/PlaceBean.java
@@ -1,33 +1,76 @@
 package com.yt.business.bean;
 
+import org.springframework.data.neo4j.annotation.Indexed;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
 import com.yt.business.common.Constants.Status;
-import com.yt.dal.hbase.BaseBean;
 import com.yt.dal.hbase.annotation.HbaseColumn;
 import com.yt.dal.hbase.annotation.HbaseTable;
+import com.yt.rsal.neo4j.bean.Neo4JBaseBean;
 
 /**
+ * 该实体定义目的地数据信息。rowkey格式为：洲（1）-国（3）-省（州）（3）-市（3），括号内是每部分长度
+ * 
+ * <p>
+ * <b>修改历史：</b>
+ * <table border="1">
+ * <tr>
+ * <th>修改时间</th>
+ * <th>修改人</th>
+ * <th>备注</th>
+ * </tr>
+ * <tr>
+ * <td>2015年6月2日</td>
+ * <td>Tony.Zhang</td>
+ * <td>Create</td>
+ * </tr>
+ * <tr>
+ * <td>2015年6月28日</td>
+ * <td>John Peng</td>
+ * <td>根据定稿的hbase和neo4j操作进行了修改完善。</td>
+ * </tr>
+ * </table>
  * 
  * @author Tony.Zhang
- * 该实体定义目的地数据信息。rowkey格式为：洲（1）-国（3）-省（州）（3）-市（3），括号内是每部分长度
+ * 
+ * @version 1.0
+ * @since 1.0
  */
 @HbaseTable(name = "T_SYS_PLACE_INFO")
-public class PlaceBean extends BaseBean {
+@NodeEntity
+public class PlaceBean extends Neo4JBaseBean {
 	private static final long serialVersionUID = -6977525800090683657L;
-	
-	private 	@HbaseColumn(name = "zname")	String continentName = ""; //洲名
-	private 	@HbaseColumn(name = "nname")	String nationalName = ""; //国名
-	private 	@HbaseColumn(name = "sname")	String stateName = ""; //州名、省名或者直辖市名
-	private 	@HbaseColumn(name = "cname")	String cityName = ""; //市名
-	
-	private	@HbaseColumn(name = "recm")		int recommended = 0; //是否推荐 0:不推荐 1：推荐
-	
-	private 	@HbaseColumn(name = "cuid")	String createdUserId = "";
-	private 	@HbaseColumn(name = "ct")		long createdTime;
-	private 	@HbaseColumn(name = "uuid")	String updatedUserId = "";
-	private 	@HbaseColumn(name = "ut")		long updatedTime;
-	private 	@HbaseColumn(name = "stat")		Status	status;
-	
+
+	private @HbaseColumn(name = "zname")
+	@Indexed
+	String continentName = ""; // 洲名
+	private @HbaseColumn(name = "nname")
+	@Indexed
+	String nationalName = ""; // 国名
+	private @HbaseColumn(name = "sname")
+	@Indexed
+	String stateName = ""; // 州名、省名或者直辖市名
+	private @HbaseColumn(name = "cname")
+	@Indexed
+	String cityName = ""; // 市名
+
+	private @HbaseColumn(name = "recm")
+	int recommended = 0; // 是否推荐 0:不推荐 1：推荐
+
+	private @HbaseColumn(name = "cuid")
+	transient String createdUserId = "";
+	private @HbaseColumn(name = "ct")
+	transient long createdTime;
+	private @HbaseColumn(name = "uuid")
+	transient String updatedUserId = "";
+	private @HbaseColumn(name = "ut")
+	transient long updatedTime;
+	private @HbaseColumn(name = "stat")
+	@Indexed
+	Status status;
+
 	public PlaceBean() {
+		super();
 	}
 
 	public String getContinentName() {
