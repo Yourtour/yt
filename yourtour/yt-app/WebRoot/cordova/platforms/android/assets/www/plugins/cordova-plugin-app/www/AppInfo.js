22dd35138cd116214608a1f061aeb8aa8c7d47d2
zhanglinping <914766813@qq.com>
2016-03-17 09:02:29 +0800

    APP提交代码整理

--- yourtour/yt-app/WebRoot/cordova/platforms/android/assets/www/plugins/cordova-plugin-app/www/AppInfo.js
+++
@@ -1,6 +0,0 @@
-cordova.define("cordova-plugin-app.AppInfo", function(require, exports, module) { var exec = require('cordova/exec');
-
-exports.getVersion = function(success, error) {
-    exec(success, error,"AppInfo","getVersion", []);
-};
-});
