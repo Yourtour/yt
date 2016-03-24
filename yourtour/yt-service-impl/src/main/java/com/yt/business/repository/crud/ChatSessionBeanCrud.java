d29ef176e5c2a1469bd55d34b49922be48380a96
John.Peng <josh_73_9@hotmail.com>
2016-03-24 17:21:00 +0800

    重新生成了CRUD operate代码。

--- yourtour/yt-service-impl/src/main/java/com/yt/business/repository/crud/ChatSessionBeanCrud.java
+++
@@ -1,8 +0,0 @@
-package com.yt.business.repository.crud;
-
-import com.yt.neo4j.repository.CrudGeneralOperate;
-import org.springframework.stereotype.Service;
-import com.yt.business.bean.ChatRoomBean;
-
-@Service
-public class ChatSessionBeanCrud extends CrudGeneralOperate<ChatRoomBean> { }
